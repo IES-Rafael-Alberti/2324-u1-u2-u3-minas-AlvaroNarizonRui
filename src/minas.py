87ef1db0db6799628268f4220156af6ad4d25ccd
AlvaroNarizonRui <aruiz@iesrafaelalberti.es>
2023-11-17 09:56:19 +0100

    Realizadas las siguientes funciones: 	1. generarMinas 	2. generarTablero

--- src/minas.py
+++ src/minas.py
@@ -12,8 +12,49 @@ import random
 def jugar():
     """
     Esta función ejecuta el juego.
-
+    Las minas se representan con el símbolo "*"
+    Las posiciones abyacentes a las minas son los números : 1,2,3,4
+    Las celdas vacias se representan con una cadena vacia : ""
+    Las banderas se representan con: "F"
     """
+    tablero = generarTablero()
+    generarMinas(tablero)
+    
+
+def generarTablero():
+    tablero = [[".",".",".",".",".",".",".","."],
+               [".",".",".",".",".",".",".","."],
+               [".",".",".",".",".",".",".","."],
+               [".",".",".",".",".",".",".","."],
+               [".",".",".",".",".",".",".","."],
+               [".",".",".",".",".",".",".","."],
+               [".",".",".",".",".",".",".","."],
+               [".",".",".",".",".",".",".","."]]
+    return tablero
+
+    
+def generarMinas(tablero:list):
+    """
+    Inserta 10 minas de forma aleatoria sobre un tablero de buscaminas de 8x8.
+    Mientras el numero de minas sea mayor que 0, se añadirá una mina en una posición 
+    aleatoria.
+
+    :param tablero: El tablero de juego donde se va a añadir las minas
+    :type tablero: list.
+    """
+    NUMEROMINAS = 10
+    while NUMEROMINAS > 0:
+        posicion_x = random.randint(0,7)
+        posicion_y = random.randint(0,7)
+        tablero[posicion_y].insert(posicion_x,"*")
+        NUMEROMINAS -= 1
+
+            
+
+
+#def generarNumerosAbyacentes():
+
+
 
 
 if __name__ == "__main__":
